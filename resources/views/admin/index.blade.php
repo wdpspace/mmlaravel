b360b3ffc620941a0122f1adde714b44c56de778
wdpspace <wdp.space@outlook.com>
2026-08-28 09:46:07 +0400

    q

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MobileMax | Admin</title>
+</head>
+<body>
+    <h2>Admin page</h2>
+    <ul>
+        <li>CRUD</li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MobileMax Homepage</title>
+</head>
+<body>
+    <h1>Welcome to MobileMax</h1>
+    <br><br>
+    <a href="/admin">Admin</a>
+</body>
+</html>
